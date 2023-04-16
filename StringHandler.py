b71e1d81a0c9206a91807d5499975215e980a296
KirchakKirill <75274771+KirchakKirill@users.noreply.github.com>
2023-04-16 17:49:40 +0300

    add method camel for task3

--- StringHandler.py
+++ StringHandler.py
@@ -1,3 +1,16 @@
 class StringHandler:
 
-    
+    def camel(self, st):
+        tmp = None
+        res = st[0]
+        for value in st:
+            if (tmp != None):
+                if (tmp.islower()):
+                    res += value.upper()
+                else:
+                    res += value.lower()
+            tmp = res[-1]
+        print(res)
+
+str = StringHandler()
+str.camel('Утром!! было! солнечно!!!!')
