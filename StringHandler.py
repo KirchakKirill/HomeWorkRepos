9ff0186d86c9641800855f6e7700428dd096bd36
KirchakKirill <75274771+KirchakKirill@users.noreply.github.com>
2023-04-16 17:51:24 +0300

    add method shortener for task4

--- StringHandler.py
+++ StringHandler.py
@@ -1,3 +1,12 @@
 class StringHandler:
 
-    
+    def shortener(self,st):
+        while '(' in st or ')' in st:
+            left = st.rfind('(')
+            right = st.find(')', left)
+            st = st.replace(st[left:right + 1], '')
+        return st
+
+
+str = StringHandler()
+print(str.shortener('123(xyz(568)9)zvq'))
