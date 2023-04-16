e7341648ce3a8fbf8b2ea9f7146cffc959d70961
KirchakKirill <75274771+KirchakKirill@users.noreply.github.com>
2023-04-16 17:47:01 +0300

    add method search substring

--- StringHandler.py
+++ StringHandler.py
@@ -1,3 +1,12 @@
 class StringHandler:
-
-    
+    def search_substr(self, subst, st):
+        resault = st.lower().find(subst.lower())
+        if (resault != -1):
+            print("Есть контакт!")
+        else:
+            print("Мимо!")
+            
+str = StringHandler()
+str.search_substr('Кол', 'коЛокОл')
+str.search_substr('Колобок', 'колобоК')
+str.search_substr('Кол', 'Плов')
